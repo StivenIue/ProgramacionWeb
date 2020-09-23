b30e8e2e5c259a3f90bd9177d8c333101d43332c
StivenIue <jslopera@correo.iue.edu.co>
2020-09-23 03:24:52 -0500

    Creacion de rama en desarrollo

---
+++ js/dist/codigo.dev.js
@@ -0,0 +1,82 @@
+"use strict";
+
+$(document).ready(function () {
+  $(".privado").hover(function () {
+    $(".privado").addClass('resaltar');
+  });
+  $(".privado").mouseleave(function () {
+    $(".privado").removeClass('resaltar');
+  });
+});
+$(document).ready(function () {
+  $(".web").hover(function () {
+    $(".web").addClass('resaltar');
+  });
+  $(".web").mouseleave(function () {
+    $(".web").removeClass('resaltar');
+  });
+});
+$(document).ready(function () {
+  $(".red").hover(function () {
+    $(".red").addClass('resaltar');
+  });
+  $(".red").mouseleave(function () {
+    $(".red").removeClass('resaltar');
+  });
+});
+$(document).ready(function () {
+  $(".flujo").hover(function () {
+    $(".flujo").addClass('resaltar');
+  });
+  $(".flujo").mouseleave(function () {
+    $(".flujo").removeClass('resaltar');
+  });
+});
+$(document).ready(function () {
+  $(".programa").hover(function () {
+    $(".programa").addClass('resaltar');
+  });
+  $(".programa").mouseleave(function () {
+    $(".programa").removeClass('resaltar');
+  });
+});
+$(document).ready(function () {
+  $(".body").hover(function () {
+    $(".body").addClass('resaltar');
+  });
+  $(".body").mouseleave(function () {
+    $(".body").removeClass('resaltar');
+  });
+});
+$(document).ready(function () {
+  $(".java").hover(function () {
+    $(".java").addClass('resaltar');
+  });
+  $(".java").mouseleave(function () {
+    $(".java").removeClass('resaltar');
+  });
+});
+$(document).ready(function () {
+  $(".css").hover(function () {
+    $(".css").addClass('resaltar');
+  });
+  $(".css").mouseleave(function () {
+    $(".css").removeClass('resaltar');
+  });
+});
+$(document).ready(function () {
+  $(".html").hover(function () {
+    $(".html").addClass('resaltar');
+  });
+  $(".html").mouseleave(function () {
+    $(".html").removeClass('resaltar');
+  });
+});
+$(document).ready(function () {
+  $(".jquery").hover(function () {
+    $(".jquery").addClass('resaltar');
+  });
+  $(".jquery").mouseleave(function () {
+    $(".jquery").removeClass('resaltar');
+  });
+});
